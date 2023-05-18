17b7ce7a06acfebd3803f46a645869633b90891b
ywha0929 <ywha0929@gmail.com>
2023-05-18 12:17:18 +0900

    Queue

--- Queue.h
+++ Queue.h
@@ -12,9 +12,6 @@ typedef struct queue{
 Queue* initQueue();
 int size(Queue* head);
 void push(Queue* head, size_t size, void* data);
-void enqueue(Queue* head, size_t size, void* data);
-void dequeue(Queue* head);
-void clear(Queue* head);
 void pop(Queue* head);
 void* top(Queue* head);
 int isEmpty(Queue* head);
