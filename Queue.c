5b452d947103a02a130661a5e5b73067646f01f4
ywha0929 <ywha0929@gmail.com>
2023-05-18 12:11:50 +0900

    Queue.c_complete

--- Queue.c
+++ Queue.c
@@ -19,6 +19,15 @@ int size(Queue* head)
 	}
 	return i;
 }
+int isEmpty(Queue* head)
+{
+	if(size(head) == 0)
+		return 1;
+	else
+		return 0;
+
+}
+
 Queue* initQueue(size_t sizeOfData)
 {
     // List* thisList = (List*) malloc(sizeof(List));
@@ -32,149 +41,50 @@ Queue* initQueue(size_t sizeOfData)
 	return thisQueue;
 }
 
-int push(List* head, size_t size, void* data)
+void push(Queue* head, size_t size, void* data)
 {
     // List* newList = (List*) malloc(1,sizeof(List*));
-    List* newList = (List*) calloc(1,sizeof(List*));
+    Queue* newNode = (Queue*) calloc(1,sizeof(Queue*));
     // printf("size of Data : %d\n",sizeof(data));
     // newList->node = (struct list_head *) malloc(1,sizeof(struct list_head));
-    newList->node = (struct list_head *) calloc(1,sizeof(struct list_head));
-    newList->node->next = newList->node;
-    newList->node->prev = newList->node;
+    newNode->node = (struct list_head *) calloc(1,sizeof(struct list_head));
+    newNode->node->next = newNode->node;
+    newNode->node->prev = newNode->node;
     // newList->data = malloc(1,size);
-    newList->data = calloc(1,size);
-    memcpy((newList->data),data,size);
+    newNode->data = calloc(1,size);
+    memcpy((newNode->data),data,size);
     // printf("%lld\n",*((int*)newList->data));
     // printf("%lld\n",*((int*)data));
     // printf("targetList address : %p\n",newList);
-    list_add_tail((newList->node) , head->node);
+    list_add_tail((newNode->node) , head->node);
     // printf("targetList address : %p\n",newList);
-    return 0;
+    return;
 }
 
-int addNodeAfter(List* head, size_t size, void* data, int index)
+void* top(Queue* head)
 {
-	List* newList = (List*) calloc(1,sizeof(List*));
-	newList->node = (struct list_head *) calloc(1,sizeof(struct list_head));
-   	INIT_LIST_HEAD(newList->node);
-   	newList->data = calloc(1,size);
-   	memcpy((newList->data),data,size);
-    	// printf("%lld\n",*((int*)newList->data));
-   	// printf("%lld\n",*((int*)data));
-   	// printf("targetList address : %p\n",newList);
-	struct list_head* cur = NULL;
-	int i = 0;
-	list_for_each(cur, head->node)
-	{
-		if(i == index)
-		{
-			list_add(newList->node,cur);
-		}
-		i++;	
-
-	}
-	// printf("targetList address : %p\n",newList);
-	return 0;
-
-
-}
-
-int addNodeBefore(List* head, size_t size, void* data, int index)
-{
-	List* newList = (List*) calloc(1,sizeof(List*));
-	newList->node = (struct list_head *) calloc(1,sizeof(struct list_head));
-   	INIT_LIST_HEAD(newList->node);
-   	newList->data = calloc(1,size);
-   	memcpy((newList->data),data,size);
-    	// printf("%lld\n",*((int*)newList->data));
-   	// printf("%lld\n",*((int*)data));
-   	// printf("targetList address : %p\n",newList);
-	struct list_head* cur = NULL;
-	int i = 0;
-	list_for_each(cur, head->node)
-	{
-		if(i == index-1)
-		{
-			list_add(newList->node,cur);
-		}
-		i++;	
-
-	}
-	// printf("targetList address : %p\n",newList);
-	return 0;
-
-
-}
-
-void* getNode(List* head, int index)
-{
-
     struct list_head* cur;
-    int i = 0;
-
-    list_for_each(cur,head->node)
-    {
-        if(i == index)
-        {
-            List* targetList = list_entry(cur,List,node)-2;
-            // printf("targetList address : %p\n",targetList);
-            // printf("%lld\n",*((int*)targetList->data));
-            return (void*) targetList->data;
-        }
-        else
-        {
-            i++;
-        }
-    }
-    return NULL;
+    cur = head->node->prev;
+    Queue* targetNode = list_entry(cur,Queue,node)-2;
+    return targetNode->data;
 }
 
-List* _getNode(List* head, int index)
-{
-
-    struct list_head* cur;
-    int i = 0;
-
-    list_for_each(cur,head->node)
-    {
-        if(i == index)
-        {
-            List* targetList = list_entry(cur,List,node)-2;
-            // printf("targetList address : %p\n",targetList);
-            // printf("%lld\n",*((int*)targetList->data));
-            return targetList;
-        }
-        else
-        {
-            i++;
-        }
-    }
-    return NULL;
-}
-
-int removeNode(List* head, int index)
+int pop(Queue* head)
 {
     struct list_head* cur;
     int i = 0;
-    List* targetList = NULL;
-    list_for_each(cur,head->node)
-    {
-        if(i == index)
-        {
-            targetList = list_entry(cur,List,node)-2;
-            break;
-        }
-        else
-        {
-            i++;
-        }
-
-    }
-
-    if(targetList == NULL)
+    Queue* targetList = NULL;
+    cur = head->node;
+    if(cur->next == cur->prev)
     {
         return -1;
     }
+    else
+    {
+        cur = cur->prev;
+        targetList = list_entry(cur,Queue,node)-2;
+    }
+    
     // printf("%p\n",targetList->node->prev->next);
     // printf("%p\n",targetList->node->next);
     // targetList->node->prev->next = targetList->node->next;
@@ -188,49 +98,19 @@ int removeNode(List* head, int index)
     return 0;
 }
 
-void clear(List* head)
+void clear(Queue* head)
 {
     struct list_head* cur;
-    List* targetList = NULL;
+    Queue* targetNode = NULL;
     int Size = size(head);
     // int Size = size(head);
     for(int i = 0; i< Size; i++)
     {
-        targetList = list_entry(head->node->next,List,node)-2;
-        list_del(targetList->node);
-        free(targetList->node);
-        free(targetList->data);
-        free(targetList);
+        targetNode = list_entry(head->node->next,Queue,node)-2;
+        list_del(targetNode->node);
+        free(targetNode->node);
+        free(targetNode->data);
+        free(targetNode);
     }
     return;
 }
-
-void sort(List* head, int(*comaprator)())
-{
-    int Size = size(head);
-    for(int i = 0; i< Size; i++)
-    {
-        for(int j= 0; j<Size-i-1; j++)
-        {
-            // printf("%d %d\n",i,j);
-            List* firstNode = (List*)_getNode(head,j);
-            // printf("%p\n",firstNode->data);
-            List* secondNode = _getNode(head,j+1);
-            
-            if(comaprator(firstNode->data, secondNode->data) == 0)
-            {
-                struct list_head* prevNode = firstNode->node->prev;
-                struct list_head* nextNode = secondNode->node->next;
-                prevNode->next = secondNode->node;
-                secondNode->node->prev = prevNode;
-
-                secondNode->node->next = firstNode->node;
-                firstNode->node->prev = secondNode->node;
-
-                firstNode->node->next = nextNode;
-                nextNode->prev = firstNode->node;
-            }
-        }
-    }
-}
-
--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "List.h"
+#include "Queue.h"
 
 typedef struct data{
 	int number1;
@@ -18,7 +18,24 @@ int compare(Data* firstNode, Data* secondNode);
 
 int main(void)
 {
-	
+	Data data1,data2,data3;
+    data1.number1 = 1;
+    data1.number2 = 2;
+    data2.number1 = 3;
+    data2.number2 = 4;
+    data3.number1 = 5;
+    data3.number2 = 6;
+    Queue* dataQueue = initQueue();
+    push(dataQueue,sizeof(Data),&data1);
+    push(dataQueue,sizeof(Data),&data2);
+    push(dataQueue,sizeof(Data),&data3);
+    printf("%s\n",border);
+    printf("pop : \n");
+    Data* cur = top(dataQueue);
+    printf("%d %d\n",cur->number1,cur->number2);
+    pop(dataQueue);
+    cur = top(dataQueue);
+    printf("%d %d\n",cur->number1,cur->number2);
 }
 
 int compare(Data* firstNode, Data* secondNode)
